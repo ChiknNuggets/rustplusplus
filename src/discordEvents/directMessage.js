ab18a4d957c057d5288eae397b1d61ab4fba8125
ChiknNuggets <all00132@gmail.com>
2025-08-25 17:09:38 +1000

    Update directMessage.js

--- src/discordEvents/directMessage.js
+++ src/discordEvents/directMessage.js
@@ -1,22 +1,3 @@
-/*
-    Copyright (C) 2024 Alexander Emanuelsson (alexemanuelol)
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-    https://github.com/alexemanuelol/rustplusplus
-
-*/
 const DiscordVoice = require('../discordTools/discordVoice.js');
 const Translate = require('translate');
 
@@ -27,9 +8,11 @@ module.exports = {
         if (message.author.bot) return;
 
         let speakText = message.cleanContent;
+        let AltVoice = false;
         if (/[\u3400-\u9FBF]/.test(message.cleanContent)) {
             try {
                 speakText = await Translate(message.cleanContent, { from: 'zh', to: 'en' });
+                AltVoice= true
             }
             catch (e) {
                 client.log(client.intlGet(null, 'infoCap'), `Translation failed: ${e.message}`);
@@ -42,7 +25,7 @@ module.exports = {
             const instance = client.getInstance(guildId);
             if (instance && instance.blacklist['discordIds'].includes(message.author.id)) continue;
 
-            await DiscordVoice.sendDiscordVoiceMessage(guildId, speakText);
+            await DiscordVoice.sendDiscordVoiceMessage(guildId, speakText, AltVoice);
         }
 
         client.log(client.intlGet(null, 'infoCap'), client.intlGet(null, 'logDiscordMessage', {
